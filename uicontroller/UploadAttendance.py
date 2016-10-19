6f16d490eb24a2b38d11a01af6d4493e859704f5
tharinda <tharinda221@gmail.com>
2016-10-20 02:44:59 +0530

    database updated

--- uicontroller/UploadAttendance.py
+++ uicontroller/UploadAttendance.py
@@ -5,22 +5,35 @@ from __init__ import *
 
 class UploadAttendanceAPI(Resource):
     def post(self):
+        email = request.form['EmailAddress']
         file = request.files['AttendanceFile']
         filename = secure_filename(file.filename)
         # Move the file form the temporal folder to
         # the upload folder we setup
         file.save("uploadAttendance")
-        readFile("uploadAttendance")
-        sendMail(config.mail)
+        msg = readFile("uploadAttendance")
+        # sendMail(config.mail, email, msg)
+        f = open('myfile.pdf', 'w')
+        f.write(msg)  # python will convert \n to os.linesep
+        f.close()  # you can omit in most cases as the destructor will call it
         return "Email was sent"
 
 
 def readFile(fileName):
+    d = set()
     with open(fileName) as f:
         for line in f:
-            print line
+            d.add(line.split(" ")[0])
+        str = "Hi, Attended student list is given below" + "\n"
+        for item in d:
+            conn = getConnection()
+            query = 'SELECT * from students WHERE StudentID =' + "'" + item + "'"
+            student = conn.execute(query).fetchall()
+            print student
+            str = str + student[0][0] + " " + student[0][2] + "\n"
+        return str
 
-def sendMail(mail):
-    msg = Message('Hello', sender='attendance.sys.999@gmail.com', recipients=['tharinda221@gmail.com'])
-    msg.body = "This is generated message"
+def sendMail(mail, email, userMessage):
+    msg = Message('Attendance System Report', sender='tharinda2221@gmail.com', recipients=[email])
+    msg.body = userMessage
     mail.send(msg)
